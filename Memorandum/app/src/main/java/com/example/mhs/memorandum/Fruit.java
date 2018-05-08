5780760f57ae95dfe80290915df6583128401c3d
loganmhs <wenwenyau@qq.com>
2018-05-08 13:31:25 +0800

    备忘录测试连接数据库
    用litepal操作成功

--- Memorandum/app/src/main/java/com/example/mhs/memorandum/Fruit.java
+++ Memorandum/app/src/main/java/com/example/mhs/memorandum/Fruit.java
@@ -1,18 +1,29 @@
 package com.example.mhs.memorandum;
 
-public class Fruit {
+import org.litepal.crud.DataSupport;
+
+public class Fruit extends DataSupport{
     private String name;
     private String CNname;
+
     public Fruit(String name,String CNname){
         this.name=name;
         this.CNname=CNname;
     }
 
-    public String getCNname() {
-        return CNname;
+    public void setName(String name){
+        this.name=name;
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setCNname(String CNname) {
+        this.CNname = CNname;
+    }
+
+    public String getCNname() {
+        return CNname;
     }
 }
--- Memorandum/app/src/main/java/com/example/mhs/memorandum/MainActivity.java
+++ Memorandum/app/src/main/java/com/example/mhs/memorandum/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import org.litepal.LitePal;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,7 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LitePal.getDatabase();
+
+        Fruit durian=new Fruit("durian","榴莲");
+        durian.save();
     }
 }
 //实现了fragment中使用recyclerView,下一步实现recyclerView的点击
 //实现了recyclerView的点击监听，和碎片之间的通信
+//不知道是否连接上了数据库，有待检测
